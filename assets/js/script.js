c1b71e5f0640f27382fa8fef5842f2543d026c09
Serena Chan <serenacha@berkeley.edu>
2017-12-20 23:08:40 -0800

    jquert

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,27 @@
 $(document).ready(function() {
-	$('.Serena').typeIt({
-		
+
+$('#name').typeIt({
+ speed: 110,
+ loop: true
+})
+.tiPause(2000)
+.tiDelete(12)
+.tiType("Economicst")
+.tiPause(1500)
+.tiDelete(30)
+.tiPause(500)
+.tiType("Designer")
+.tiPause(1500)
+.tiDelete(30)
+.tiPause(500)
+.tiType("Innovator")
+.tiPause(1000)
+.tiDelete(30)
+.tiPause(500)
+.tiType("Food Lover")
+.tiPause(500)
+.tiDelete(30)
+.tiPause(500)
+
 });	
 	
-});
